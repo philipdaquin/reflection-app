0889f556100f5c781bd0da30ea92073c48926fdf
philipdaquin <philipdaquin20@gmail.com>
2023-03-06 18:40:50 +0800

    fixed env issues

--- whisper/src/main.rs
+++ whisper/src/main.rs
@@ -1,13 +1,15 @@
 use whisper::server::new_server;
 use whisper::error::Result;
+use dotenv::dotenv;
 
 #[actix_web::main]
 async fn main() -> Result<()> { 
+    dotenv().ok();
 
-    // let backup = std::env::var("BACKUP_PORT")
-    //     .unwrap()
-    //     .parse::<u32>()
-    //     .unwrap();
+    let backup = std::env::var("BACKUP_PORT")
+        .unwrap()
+        .parse::<u32>()
+        .unwrap();
 
     let port = std::env::var("PORT")
         .ok()
