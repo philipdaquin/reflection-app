f82b6fdd17ddbf136c778d5d8968810137983c14
philipdaquin <philipdaquin20@gmail.com>
2023-04-22 19:42:19 +0800

    audio

--- app/components/navigation/NavigationButtons.tsx
+++ app/components/navigation/NavigationButtons.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {HomeIcon, MagnifyingGlassIcon, MicrophoneIcon, 
+import React, { useState } from 'react'
+import {HomeIcon, MagnifyingGlassIcon, MicrophoneIcon, PlusIcon,
   
   UserIcon, MusicalNoteIcon,
 CalendarDaysIcon } from '@heroicons/react/24/outline'
@@ -23,25 +23,47 @@ function Button({icon, routerName}: Props) {
 
 
 function NavigationButtons() {
+
+    const [toggle, setToggle] = useState(false)
+
+    const openToggle = () => { 
+      setToggle(!toggle)
+    }
+
     return (
       // <div className='flex flex-col space-y-3 p-2 h-[244px]  justify-between items-center w-[53px]  space-x-1 rounded-[20px] bg-white shadow-xl'>
       <div className='flex md:flex-col space-y-3 md:items-center md'>
+        
         <Button 
           routerName=''
           icon={<HomeIcon height={24} width={24} color="white"/>}/>
-        <Button 
-          routerName='chat'
-          icon={<RiChatVoiceLine size={24} color="white"/>}/>
-        <Button 
-          routerName='record'
-          icon={<MicrophoneIcon height={24} width={24} color="white"/>}/>
-        <Button 
-          routerName='playlist'
-          icon={<MusicalNoteIcon height={24} width={24} color="white"/>}/>
+        
         <Button 
           routerName='weekly_calendar'
           icon={<CalendarDaysIcon height={24} width={24} color="white"/>}/>
       
+        <div onClick={openToggle} className='bg-[#424242] rounded-full p-2 cursor-pointer'>
+          <PlusIcon height={24} width={24} color="white"/>
+        </div>
+
+        {
+          toggle && (
+            <div className='space-y-3'>
+              <Button 
+                routerName='chat'
+                icon={<RiChatVoiceLine size={24} color="white"/>}/>
+              <Button 
+                routerName='record'
+                icon={<MicrophoneIcon height={24} width={24} color="white"/>}/>
+            
+            </div>
+          )
+        }
+      
+        
+        <Button 
+          routerName='playlist'
+          icon={<MusicalNoteIcon height={24} width={24} color="white"/>}/>
       
       </div>
     )
