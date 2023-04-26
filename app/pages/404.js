e2aaa05a9064dd3e03e9127698bc78dd99dc6a3c
philipdaquin <philipdaquin20@gmail.com>
2023-04-26 11:00:51 +0800

    fix

--- app/pages/404.js
+++ app/pages/404.js
@@ -1,7 +1,43 @@
+import { Head } from "next/document";
 import Home from ".";
+import ErrorPageContent from "../components/pages/ErrorPageContent";
+import PhoneView from "../components/PhoneView";
+import SwitchView from "../components/SwitchView";
+import SettingsButtons from "../components/SettingsButtons";
+import NavigationMobile from "../components/navigation/mobile/NavigationMobile";
+import HomeNav from "../components/navigation/mobile/HomeNav";
 /// temporary  error page 
 export default function _404() {
-    return ( 
-      <Home />
-    )
+  return (
+    // <>
+    //   <Head>
+    //       <title>Hello John 👋</title>
+    //       <link rel="icon" href="/favicon.ico" />
+    //   </Head>
+
+    //   <div className="md:bg-[#EEEEEE] bg-white flex 
+    //       md:min-h-[100vh] flex-col h-screen md:py-14 px-[104px]">
+    //       <main className="justify-center flex flex-col items-center space-y-[27px] ">
+    //           <div className="flex items-center md:relative md:right-10 h-full">
+    //               <PhoneView children={<ErrorPageContent/>} />
+    //           </div>
+    //           <div className='md:block hidden'>
+    //               <SwitchView />
+    //           </div>
+    //       {/* <RecordComponent /> */}
+    //       </main>
+
+    //       {/* Settings / Footer  */}
+    //       <div className="flex-grow"></div>
+    //       <div className='relative bottom-10 md:block hidden'>
+    //           <SettingsButtons />
+    //       </div>
+    //       <div className='flex items-center md:hidden justify-center mb-10'>
+    //           <NavigationMobile children={<HomeNav/>} />        
+    //       </div>
+    //   </div>
+    
+    // </>
+    <Home />
+  )
   }
