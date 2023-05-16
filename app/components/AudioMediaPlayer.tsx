b07ad85fcdbd22532058224fb8041868cc00a9dd
philipdaquin <philipdaquin20@gmail.com>
2023-05-16 17:12:34 +0800

    audio player hook

--- app/components/AudioMediaPlayer.tsx
+++ app/components/AudioMediaPlayer.tsx
@@ -1,52 +1,33 @@
 import { PlayIcon, StopIcon } from '@heroicons/react/24/solid';
-import React, { useEffect, useRef, useState } from 'react'
+import React, { MutableRefObject, useEffect, useRef, useState } from 'react'
 import ReactPlayer from 'react-player';
 import {MdOutlineForward10 ,  MdOutlineReplay10} from 'react-icons/md'
 import { HeartIcon } from '@heroicons/react/24/outline';
-import {BsRepeat1} from 'react-icons/bs'
-
+import {BsRepeat, BsRepeat1} from 'react-icons/bs'
+import { useAudioPlayer } from '../hooks/useAudioPlayer';
 
 interface Props { 
     src: string
-
 }
 function AudioMediaPlayer({src}: Props) {
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [currentTime, setCurrentTime] = useState(0);
-    const [duration, setDuration] = useState(0);
-  
-    const handlePlayClick = () => {
-      setIsPlaying(!isPlaying);
-    };
-    const handleProgress = (state: { played: number; playedSeconds: number }) => {
-      setCurrentTime(state.playedSeconds);
-    };
-  
-    const handleDuration = (duration: number) => {
-      setDuration(duration);
-    };
+    const {
+      isPlaying,
+        currentTime,
+        setCurrent,
+        isLoop,
+        handlePlayerLoop,
+        duration,
+        playerRef,
+        handleProgress,
+        handlePlayClick,
+        handleSliderChange,
+        handleDuration,
+        handleFastForward,
+        handleRewindBack,
+        formatTime,
+    } = useAudioPlayer(src)
     
-    const audioPlayer = useRef<ReactPlayer | null>(null)
-    
-
-    const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      audioPlayer?.current?.seekTo(parseFloat(event.target.value))
-    };
-
-    const handleFastForward = () => {
-      audioPlayer?.current?.seekTo(audioPlayer?.current?.getCurrentTime() + 10)
-    };
-
-    const handleRewindBack = () => {
-      audioPlayer?.current?.seekTo(audioPlayer?.current?.getCurrentTime() - 10)
-    };
-    const formatTime = (time: number) => {
-      const minutes = Math.floor(time / 60);
-      const seconds = Math.floor(time % 60)
-        .toString()
-        .padStart(2, "0");
-      return `${minutes}:${seconds}`;
-    };
+    const onHover = "hover:bg-[#EDECEC] active:bg-[#E0E0E0] rounded-full p-2"
 
     return (
         <> 
@@ -56,17 +37,18 @@ function AudioMediaPlayer({src}: Props) {
                     min={0}
                     max={duration}
                     value={currentTime}
+
                     onChange={handleSliderChange}
                     className="w-full "
                 />
                 <ReactPlayer
-                    ref={audioPlayer}
+                    ref={playerRef}
                     url={src}
                     playing={isPlaying}
-                    onSeek={setCurrentTime}
+                    onSeek={setCurrent}
                     onProgress={handleProgress}
                     onDuration={handleDuration}
-
+                    loop={isLoop}
                     style={{ 
                     display: "none", 
                     width: "100%" 
@@ -77,28 +59,42 @@ function AudioMediaPlayer({src}: Props) {
           <div className="flex items-center flex-row justify-between w-full text-[#9e9e9e] text-[9px] text-left font-bold">
             <span>{formatTime(currentTime)}</span>
             <span>{formatTime(duration)}</span>
+
           </div>
 
           <div className='flex flex-row items-center justify-between w-full pt-[20px] '>
-
-            <HeartIcon height={25} width={25} color="#424242" strokeWidth={2} />
             
-            <div onClick={handleRewindBack} className={"cursor-pointer"}>
+            <div className={`cursor-pointer ${onHover}`}>
+              <HeartIcon height={25} width={25} color="#424242" strokeWidth={2} />
+            </div>
+            
+            <div onClick={handleRewindBack} 
+              className={`cursor-pointer ${onHover}`}>
                 <MdOutlineReplay10 size={30} color="#424242"/>    
             </div>
             
             
-            <button onClick={handlePlayClick} className="items-center flex justify-center bg-[#5d5fef] rounded-full p-[22px] ">
+            <button onClick={handlePlayClick} 
+              className="items-center flex justify-center bg-[#5d5fef] rounded-full p-[22px] ">
                 {!isPlaying ? 
                 <PlayIcon height={25} width={25} color="#fff" /> : 
                 <StopIcon height={25} width={25} color="#fff"/>}
             </button>
             
-            <div onClick={handleFastForward} className={"cursor-pointer"}>
+            <div onClick={handleFastForward} 
+              className={`cursor-pointer ${onHover}`}>
                 <MdOutlineForward10 size={30} color="#424242"/>    
             </div>
             
-            <BsRepeat1 size={30} color="#424242"/>    
+            <div className={`cursor-pointer ${onHover}`} onClick={handlePlayerLoop}>
+              {
+                isLoop ? (
+                  <BsRepeat1 size={30} color="#424242"/>    
+                ) : (
+                  <BsRepeat size={30} color="#424242"/>    
+                )
+              }
+            </div>
           </div>
         </div>
         </>
