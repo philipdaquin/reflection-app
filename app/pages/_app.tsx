5aa519dc82c26f072a8397a89d18bb9bc8689031
philipdaquin <philipdaquin20@gmail.com>
2023-05-18 14:35:03 +0800

    animation

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -13,11 +13,23 @@ function MyApp({ Component, pageProps, router }: AppProps) {
   return (
       <RecoilRoot>
         {/* <AuthProvider> */}
-        <Layout>
-          <AudioProvider>
-              <Component {...pageProps} key={router.route} />
-          </AudioProvider>
-        </Layout>
+        <AnimatePresence
+            // Disable any initial animations on children that
+            // are present when the component is first rendered
+            initial={false}
+            // Only render one component at a time.
+            // The exiting component will finish its exit
+            // animation before entering component is rendered
+            mode={'sync'}
+            // Fires when all exiting nodes have completed animating out
+            onExitComplete={() => null}
+        >
+          <Layout>
+            <AudioProvider>
+                <Component {...pageProps} key={router.route} />
+            </AudioProvider>
+          </Layout>
+        </AnimatePresence>
         {/* </AuthProvider> */}
       </RecoilRoot>
   )
