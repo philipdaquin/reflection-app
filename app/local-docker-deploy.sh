72a0273a137ea24a23f4100d0e2d2d43fa97a4ca
philipdaquin <philipdaquin20@gmail.com>
2023-10-18 12:53:27 +0800

    automation client side

---
+++ app/local-docker-deploy.sh
@@ -0,0 +1,19 @@
+
+# Kill and Delete Current Instances
+docker kill reflection-app
+docker rm reflection-app
+
+# install dependencies
+npm i --legacy-peer-deps
+
+# Build App
+npm run build 
+
+# Build Image 
+docker build -t reflection-app . 
+
+# Tag Image
+docker tag reflection-app philipasd/reflection-app:0.0.0
+
+# Publish Image 
+docker push philipasd/reflection-app:0.0.0
